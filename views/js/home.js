d60619b69ae7c39d03377816c03192fbca7f7914
PedroSousa2020 <sousapedro747@gmail.com>
2020-08-01 14:01:56 +0100

    Demo header builded

--- views/js/home.js
+++ views/js/home.js
@@ -1,19 +1,44 @@
-//Header opacity
 
+//Header opacity
+var cont=5;
+var d;
+
+window.addEventListener('load',() => {
+        
+    $('.pre-loader-container').fadeOut(4000);
+    d = setInterval(() => { if(cont>9) cont=6; else cont++;;   $('.animback').css('background-image', "url(/views/content/img/"+cont+".jpg)");} ,5000)
+    $('.scroller').fadeOut(1000);
+
+});
 
 window.addEventListener('scroll', () => {
-
     const windowTop = window.pageYOffset;
-
-        //$('navbar-1').style.opacity = "0";
-
+        
         if(windowTop > 100){
-           $('.navbar-2').removeClass('hidden');
-           $('.navbar-2').fadeIn(999);
+
+
+            $('.navbar-2').removeClass('hidden');
+            $('.navbar-2').fadeIn(999);
+
         }else{
             $('.navbar-2').fadeOut();
-            //$('.navbar-2').addClass('hidden');
+
         }
 
+        if(windowTop>150){
+
+            $('.welcome-sms').css('animation', 'none')
+            $('#frase').css('animation', 'none')
+            
+            $('.welcome-sms').hide(2000);
+        }
+        else
+            $('.welcome-sms').show(100)
+
+        if(windowTop>250){
+            $('.scroller').fadeIn(1000);
+            $('.scroller').on('click', () => $('html, body').animate({scrollTop:0}, 1000));
+        }else
+            $('.scroller').fadeOut(100);
      
-})    
+});  
